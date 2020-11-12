b9a107b39a6814ce9312d3f89661031b5c82889d
GViveen <glenn.viveen@student.ru.nl>
2020-11-12 10:17:12 +0100

    Update CausalDiagram_4.R
    Added cooridnates for a better viewing experience of the DAG.

--- CausalDiagram_4.R
+++ CausalDiagram_4.R
@@ -44,23 +44,79 @@ d<- d.ordering
 # Extract polychoric correlation matrix
 M <- lavCor(d)
 
-g <- dagitty("dag {
-    Pedu -> {famsize address internet paid activities higher studytime}
-    Pstatus -> {famsize famrel absences alc}
-    age -> {goout alc higher absences}
-    sex -> {alc health higher goout studytime activities}
-    famsize -> {freetime studytime famrel}
-    address -> {activities internet}
-    internet -> {activities goout higher}
-    famrel -> {alc goout activities health}
-    freetime -> {studytime goout health}
-    studytime -> {freetime health higher absences}
-    paid -> {studytime higher}
-    activities -> {studytime higher}
-    goout -> {alc higher absences}
-    alc -> {health higher absences studytime}
-    absences -> higher
-}")
+g <- dagitty('dag {
+bb="-4.293,-6.345,4.769,5.066"
+Pedu [pos="3.221,-5.140"]
+Pstatus [pos="1.985,-5.132"]
+absences [pos="0.290,0.863"]
+activities [pos="2.685,-2.492"]
+address [pos="3.818,-4.017"]
+age [pos="0.843,-5.125"]
+alc [pos="-1.535,-1.599"]
+famrel [pos="1.466,-3.548"]
+famsize [pos="2.823,-3.994"]
+freetime [pos="0.947,-0.476"]
+goout [pos="0.091,-2.417"]
+health [pos="-0.774,1.414"]
+higher [pos="1.466,1.562"]
+internet [pos="3.610,-3.317"]
+paid [pos="3.368,-2.507"]
+sex [pos="-0.021,-5.118"]
+studytime [pos="-0.246,-0.491"]
+Pedu -> activities
+Pedu -> address
+Pedu -> famsize
+Pedu -> higher
+Pedu -> internet
+Pedu -> paid
+Pedu -> studytime
+Pstatus -> absences
+Pstatus -> alc
+Pstatus -> famrel
+Pstatus -> famsize
+absences -> higher
+activities -> higher
+activities -> studytime
+address -> activities
+address -> internet
+age -> absences
+age -> alc
+age -> goout
+age -> higher
+alc -> absences
+alc -> health
+alc -> higher
+alc -> studytime
+famrel -> activities
+famrel -> alc
+famrel -> goout
+famrel -> health
+famsize -> famrel
+famsize -> freetime
+famsize -> studytime
+freetime -> goout
+freetime -> health
+freetime -> studytime
+goout -> absences
+goout -> alc
+goout -> higher
+internet -> activities
+internet -> goout
+internet -> higher
+paid -> higher
+paid -> studytime
+sex -> activities
+sex -> alc
+sex -> goout
+sex -> health
+sex -> higher
+sex -> studytime
+studytime -> absences
+studytime -> freetime
+studytime -> health
+studytime -> higher
+}
+')
 
 localTests( g, sample.cov=M, sample.nobs=nrow(d) )
 
